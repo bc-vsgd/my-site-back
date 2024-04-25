f2c1a9e30f084a0fcd4d4943e2b1e982807473a4
bc-vsgd <benoit.charlesbc@yahoo.fr>
2024-04-25 10:23:42 +0200

    Home route: get visits by author id & other visits

--- routes/visits/home.js
+++ routes/visits/home.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+// Mongoose
+// const mongoose = require("mongoose");
 // Models
 const Visit = require("../../models/visits/Visit");
 
@@ -10,6 +12,45 @@ router.get("/visits", async (req, res) => {
       return res.status(200).json({ message: "Home page", data: foundVisits });
     } else {
       return res.status(200).json({ message: "Home page, no visits for now" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/visits/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log("id: ", id);
+    const foundVisits = await Visit.find();
+
+    const authorVisits = await Visit.find()
+      .populate("author", "_id")
+      .find({ author: { _id: id } });
+    if (foundVisits) {
+      const otherVisits = [];
+      for (let i = 0; i < foundVisits.length; i++) {
+        const visit = foundVisits[i];
+        console.log("visit id: ", visit._id.toString());
+        console.log("author id: ", visit.author._id.toString());
+        console.log("params id: ", id);
+        if (visit.author._id.toString() === id) {
+          console.log("id ok");
+        } else {
+          console.log("id not ok");
+          otherVisits.push(visit);
+        }
+      }
+      return res.status(200).json({
+        message: "Home page",
+        foundVisits: foundVisits,
+        authorVisits: authorVisits,
+        otherVisits: otherVisits,
+      });
+    }
+    // No visit
+    else {
+      return res.status(200).json({ message: "Home page, no visit for now" });
     }
   } catch (error) {
     return res.status(500).json({ message: error.message });
